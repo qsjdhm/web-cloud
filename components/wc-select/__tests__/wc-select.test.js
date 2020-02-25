8704e421c4437253c0266400530d9daa20ed419c
DESKTOP-TR0K2SP\admin <zhangyan1@vtstar.net>
2020-02-25 12:00:58 +0800

    no message

--- components/wc-select/__tests__/wc-select.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const wcSelect = require('..');
-
-describe('@wc/wc-select', () => {
-    it('needs tests');
-});
--- components/wc-select/lib/wc-select.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = wcSelect;
-
-function wcSelect() {
-    // TODO
-}
